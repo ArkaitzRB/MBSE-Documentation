727e2795f1113722eb95a1754b247046aab19230
ArkaitzRB <arkaitzrb@hotmail.com>
2022-02-07 08:02:54 +0100

    Update Json and Images

--- js/app.js
+++ js/app.js
@@ -3,16 +3,12 @@ const dataError = false;
 
 document.addEventListener("DOMContentLoaded", () => {
     let data = readData(jsonFile)
-    console.log(data)
-    drawCard(data);
+    //data = JSON.parse('[{"title": "Rhapsody","descr": "Descripción de conceptos básicos en Rhapsody","url": "","image": "./img/rhapsody.png"},{"title": "Getting Started with Rhapsody","descr": "Rhapsody Solution: SysML project, Use Case, Requirements, ...","url": "","image": "./img/sysML.png"},{"title": "","descr": "","url": "https://www.google.es","image": ""}]')
+    drawCards(data);
 })
 
 function readData(jsonFile) {
     try {
-        //let jsonTxt = '{"title":"Title", "descr":"Description", "url":"https://www.google.com", "image":"./img/icon_en.png"}';
-        //let jsonData = JSON.parse(jsonTxt);
-        //return jsonData
-        
         let data = fetch(jsonFile).then(response => {
                             return response.json();
                         }).then(data => {
@@ -23,12 +19,6 @@ function readData(jsonFile) {
                             console.error('Error:', error);
                             dataError = true;
                         });
-        console.log(data)
-
-        console.log(data)
-        console.log(data.title)
-        console.log(data.descr)
-        console.log(data.url)
 
         return data
 
@@ -37,24 +27,35 @@ function readData(jsonFile) {
     }
 } 
 
-function drawCard(data) {
-    const template = document.getElementById("card").content;
-    const clone = template.cloneNode(true);
+function drawCards(data) {
+    if (Array.isArray(data)){
+        data.forEach(element => drawCard(element));
+    }
+    else
+    {
+        drawCard(data);
+    }
+}
 
-    clone.querySelector(".image").setAttribute("src", data.image);
-    //clone.querySelector(".title").innerHTML = `${data.title} <span>${pokemon.descr}hp</span>`;
-    clone.querySelector(".title").textContent = data.title;
-    clone.querySelector(".descr").textContent = data.descr;
-    var button = clone.querySelector("button");
-    //Add event handler
-    button.addEventListener ("click", function() {
-        window.open(data.url, "_newtab" );
-        //window.open (data.url, "_blank" );
-    });
-
+function drawCard(element) {
+    if (element.title !== ""){
+        const template = document.getElementById("card").content;
+        const clone = template.cloneNode(true);
     
-    const fragment = document.createDocumentFragment();
-    fragment.appendChild(clone);
-    const grid = document.getElementById("grid");
-    grid.appendChild(fragment);
+        clone.querySelector(".image").setAttribute("src", element.image);
+        //clone.querySelector(".title").innerHTML = `${element.title} <span>${pokemon.descr}hp</span>`;
+        clone.querySelector(".title").textContent = element.title;
+        clone.querySelector(".descr").textContent = element.descr;
+        var button = clone.querySelector("button");
+        //Add event handler
+        button.addEventListener ("click", function() {
+            window.open(element.url, "_newtab" );
+            //window.open (element.url, "_blank" );
+        });
+    
+        const fragment = document.createDocumentFragment();
+        fragment.appendChild(clone);
+        const grid = document.getElementById("grid");
+        grid.appendChild(fragment);
+    }
 }
